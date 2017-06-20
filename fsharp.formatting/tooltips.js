701a41b0637e0dcd75d962528aaab935bc81f278
evelinag <evelina@evelinag.com>
2017-06-20 15:09:21 +0100

    Adding Stack Overflow analysis blog and some other fixes

--- fsharp.formatting/tooltips.js
+++ fsharp.formatting/tooltips.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var currentTip = null;
 var currentTipElement = null;
 
@@ -41,4 +42,49 @@ function showTip(evt, name, unique, owner) {
     el.style.left = posx + "px";
     el.style.top = posy + "px";
     el.style.display = "block";
+=======
+var currentTip = null;
+var currentTipElement = null;
+
+function hideTip(evt, name, unique) {
+    var el = document.getElementById(name);
+    el.style.display = "none";
+    currentTip = null;
+}
+
+function findPos(obj) {
+    var curleft = 0;
+    var curtop = obj.offsetHeight;
+    while (obj) {
+        var pos = $(obj).css("position");
+        if (pos == "relative" || pos == "absolute") break;
+        curleft += obj.offsetLeft;
+        curtop += obj.offsetTop;
+        obj = obj.offsetParent;
+    };
+    return [curleft, curtop];
+}
+
+function hideUsingEsc(e) {
+    if (!e) { e = event; }
+    hideTip(e, currentTipElement, currentTip);
+}
+
+function showTip(evt, name, unique, owner) {
+    document.onkeydown = hideUsingEsc;
+    if (currentTip == unique) return;
+    currentTip = unique;
+    currentTipElement = name;
+
+    var pos = findPos(owner ? owner : (evt.srcElement ? evt.srcElement : evt.target));
+    var posx = pos[0];
+    var posy = pos[1];
+
+    var el = document.getElementById(name);
+    var parent = (document.documentElement == null) ? document.body : document.documentElement;
+    el.style.position = "absolute";
+    el.style.left = posx + "px";
+    el.style.top = posy + "px";
+    el.style.display = "block";
+>>>>>>> 0656903f72ba9199d5ab740829bbc0ff87ae5a8c
 }
